5a3b75333b4f506c07baf48fca7ba980f8c28f4a
Olivier <orohellec44@gmail.com>
2018-12-03 17:57:50 +0100

    tables finish

--- app/models/cards_discipline.rb
+++ app/models/cards_discipline.rb
@@ -1,2 +1,4 @@
 class CardsDiscipline < ApplicationRecord
+  belongs_to :cards
+  belongs_to :disciplines
 end
--- app/models/spoken_language.rb
+++ app/models/spoken_language.rb
@@ -1,2 +1,3 @@
 class SpokenLanguage < ApplicationRecord
+  has_many :cards, through: :cards_languages
 end
