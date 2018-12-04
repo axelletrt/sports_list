46864619f5684fa37a83dc11517ce07ccf0f2509
Axelle Tortai <a.tortai14@gmail.com>
2018-12-05 00:13:48 +0100

    Get lat and lng

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -12,9 +12,8 @@ class CardsController < ApplicationController
 
   def create
   	@cards = Card.new
-    @cards.professional_id = current_professional.id
-	  puts params
-    @cards.discipline_id = Discipline.id
+    @cards.professional_id = current_professional.id 
+    # @cards.discipline_id = Discipline.id
   	@cards.activity_title = params["card"][:activity_title]
   	@cards.short_description = params["card"][:short_description]
   	@cards.long_description = params["card"][:long_description]
@@ -24,13 +23,14 @@ class CardsController < ApplicationController
   	@cards.country = params["card"][:country]
   	@cards.price = params["card"][:price]
   	@cards.length = params["card"][:length]
-
   	@cards.opening_hour = params["appt"]
   	@cards.closing_hour = params["appt2"]
   	@cards.whatsapp = params["card"][:whatsapp]
   	@cards.website = params["card"][:website]
   	@cards.facebook = params["card"][:facebook]
-  	@cards.instagram = params["card"][:instagram]
+		@cards.instagram = params["card"][:instagram]
+		@cards.latitude = params["lat"]
+		@cards.longitude = params["lng"]
   	@cards.save
 
   	puts  @cards.errors.full_messages
