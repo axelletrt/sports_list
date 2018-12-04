fa8ec572890abe5e8c58e0a4868ce98fbe5d3801
Olivier <orohellec44@gmail.com>
2018-12-04 12:54:41 +0100

    Controller discipline done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'disciplines/index'
+  get 'disciplines/show/:id', to: "disciplines#show"
+  get 'disciplines/yoga'
+  get 'disciplines/surf'
+  get 'disciplines/scuba_diving'
   devise_for :professionals
 
   root to: "static#Index"
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,25 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+1.times do
+  SpokenLanguage.create(name: "Anglais")
+  SpokenLanguage.create(name: "Espagnol")
+  SpokenLanguage.create(name: "Français")
+end
+
+1.times do
+  Discipline.create!(name: "surf")
+  Discipline.create!(name: "yoga")
+  Discipline.create!(name: "scuba diving")
+end
+
+index = 1
+60.times do
+  user = Professional.create! :first_name => 'John', :last_name => "poufi #{index}", :email => "john#{index}@gmail.com", :password => 'topsecret', :password_confirmation => 'topsecret'
+  index += 1
+end
+
 index = 1
 index0 = 0
 index1 = 1
@@ -13,13 +32,30 @@ index2 = 2
 
 20.times do
 card = Card.new(activity_title: "surf + #{index}", long_description: "je suis une longue description numéro #{index}",
-      organization: index2, city: index2, country: 0, price: "45", length: "23:59:59.9999999", professional: index)
+      organization: index2, city: index2, country: 0, price: "45", length: "23:59:59.9999999", professional_id: index)
 card.save
 card = Card.new(activity_title: "surf + #{index}", long_description: "je suis une longue description numéro #{index}",
-      organization: index1, city: index2, country: 0, price: "45", length: "23:59:59.9999999", professional: index)
+      organization: index1, city: index2, country: 0, price: "45", length: "23:59:59.9999999", professional_id: index)
 card.save
 card = Card.new(activity_title: "surf + #{index}", long_description: "je suis une longue description numéro #{index}",
-      organization: index0, city: index2, country: 0, price: "45", length: "23:59:59.9999999", professional: index)
+      organization: index0, city: index2, country: 0, price: "45", length: "23:59:59.9999999", professional_id: index)
 card.save
 index += 1
+end
+
+index = 1
+20.times do
+  CardsDiscipline.create!(card_id: index, discipline_id: 1)
+  index += 1
+end
+
+20.times do
+  CardsDiscipline.create!(card_id: index, discipline_id: 2)
+  index += 1
+end
+
+10.times do
+  CardsDiscipline.create!(card_id: index, discipline_id: 2)
+  CardsDiscipline.create!(card_id: index, discipline_id: 3)
+  index += 1
 end
