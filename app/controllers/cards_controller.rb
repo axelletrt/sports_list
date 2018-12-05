4c6c794fae13c583e54bde112740e91c8ab33828
Shayane08 <syakataly@gmail.com>
2018-12-05 18:01:35 +0100

    form card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,7 +2,7 @@ class CardsController < ApplicationController
 
   def index
 
-	end
+end
 	
   def show
   	@cards = Card.find(params[:id])
@@ -14,31 +14,27 @@ class CardsController < ApplicationController
 
   def edit 
   	@cards = Card.find(params[:id])
-
   end
 
 
   def update
   	@cards = Card.find(params[:id])
-  	@cards.update(long_description: params["cards"][:long_description], short_description: params["cards"][:short_description])
-
-
-
+  	cards_params = params.require(:card).permit(:long_description, :short_description)
+  	@cards.update(cards_params)
   end
 
-	def create 
+      
+
+  def create 
 		@card = Card.new(card_parameters)
 		@card.professional_id = current_professional.id
 		@card.discipline_id  = params["card"]["id"]
 		@card.opening_hour = params["appt"]
-		@card.closing_hour = params["appt2"] 
+		@card.closing_hour = params["appt2"]
 		@card.latitude = params["lat"]
 		@card.longitude = params["lng"]
-		puts "ok"
-
-		puts params[:card][:id]
 		@card.save
-	end
+  end
 	
 	private 
   
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'home/index'
   devise_for :professionals
   
-  resources :cards, only: [:index, :create, :new, :edit]
+  resources :cards, only: [:index, :create, :new, :edit, :update]
 
   resources :disciplines, only: [:index, :show]
   get 'disciplines/categorie/:id', to: 'disciplines#categorie'
