d72a2946836972758c72fe9f7a578ba1781339f4
Olivier <orohellec44@gmail.com>
2018-12-07 12:35:47 +0100

    a user can comment a card only one time

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,7 +1,12 @@
 class EvaluationsController < ApplicationController
   def create
     p_eval = params[:evaluation]
-    Evaluation.create(comment: p_eval[:content], eval: p_eval[:eval], card_id: params[:id], user_id: current_user.id)
-    redirect_to "/cards/#{params[:id]}"
+    # search in the database Evaluations if the user has already done a comment on this card
+    if (Evaluation.where(user_id: current_user.id, card_id: params[:id]).empty?)
+      Evaluation.create(comment: p_eval[:content], eval: p_eval[:eval], card_id: params[:id], user_id: current_user.id)
+      redirect_to "/cards/#{params[:id]}"
+    else
+      puts "you can't"
+    end
   end
 end
