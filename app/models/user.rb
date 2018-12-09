e88dc38021d98a23d913ac37d77ef930f6da1519
belette06 <cyril.avronsart@gmail.com>
2018-12-09 03:34:47 +0100

    storage fini and devise fini

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :avatar_user
 
-  validates :username, presence: true
+  #validates :username, presence: true
   
 
   devise :database_authenticatable, :registerable,
