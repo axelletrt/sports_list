4ab50f428818ef946a417f6fd4cc395185a15e31
Axelle Tortai <a.tortai14@gmail.com>
2018-12-12 18:25:07 +0100

    rating stars

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 class HomeController < ApplicationController
+  
   def index
-
     @disciplines = Discipline.all 
-     @search = Search.new
-   
+    @search = Search.new
+    @card = Card.all 
+    sport_cards_id = CardsDiscipline.where(discipline_id: params[:id]).pluck(:card_id)
+    @cards = @card.find(sport_cards_id)
   end
 
  
