eedca8b1c1b1afa2dbec82ea7c6d7fe9e25dd456
Olivier <orohellec44@gmail.com>
2018-12-14 11:48:37 +0100

    take notes of dorian suggestion

--- app/controllers/disciplines_controller.rb
+++ app/controllers/disciplines_controller.rb
@@ -6,11 +6,10 @@ class DisciplinesController < ApplicationController
   end
 
   def show
-    @card = Card.all
     @discipline_name = Discipline.find(params[:id]).name
     sport_cards_id = CardsDiscipline.where(discipline_id: params[:id]).pluck(:card_id)
     # only sport cards asked by the visitor
-    @cards = @card.find(sport_cards_id)
+    @cards = Card.all.find(sport_cards_id)
     @discipline_name = Discipline.find(params[:id]).name
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,8 @@ class HomeController < ApplicationController
     @cards = Card.all
     @disciplines = Discipline.all
     @search = Search.new
-    surf_cards_id = CardsDiscipline.where(discipline_id: Discipline.where(name: 'surf')).pluck(:card_id)
-    yoga_cards_id = CardsDiscipline.where(discipline_id: Discipline.where(name: 'yoga')).pluck(:card_id)
-    scuba_cards_id = CardsDiscipline.where(discipline_id: Discipline.where(name: 'scuba diving')).pluck(:card_id)
-    @cards_surf = @cards.find(surf_cards_id)
-    @cards_yoga = @cards.find(yoga_cards_id)
-    @cards_scuba = @cards.find(scuba_cards_id)
+    @cards_surf = Discipline.find_by(name: 'surf')&.cards
+    @cards_yoga = Discipline.find_by(name: 'yoga')&.cards
+    @cards_scuba = Discipline.find_by(name: 'scuba diving')&.cards
   end
 end
