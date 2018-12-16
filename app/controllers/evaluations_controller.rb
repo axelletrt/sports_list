a060e18969ca6ec8baee0090fbd4123aff91d3d0
Axelle Tortai <a.tortai14@gmail.com>
2018-12-17 00:12:54 +0100

    back updates

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,5 +1,3 @@
-
-
 class EvaluationsController < ApplicationController
   
     def index 
@@ -17,14 +15,8 @@ class EvaluationsController < ApplicationController
     def create
       p_eval = params[:evaluation]
       # search in the database Evaluations if the user has already done a comment on this card
-      # (Evaluation.where(user_id: current_user.id, card_id: params[:id]).empty?)
-        @evaluation = Evaluation.create(comment: p_eval[:content], eval: p_eval[:eval], card_id: params[:id], user_id: current_user.id)
-        redirect_to "/cards/#{params[:id]}"
-
-        if @evaluation.save
-          respond_to do |format|
-             format.js   ## cela va rendre create.js.erb
-          end
-        end
-    end
+      (Evaluation.where(user_id: current_user.id, card_id: params[:id]).empty?)
+      @evaluation = Evaluation.create(comment: p_eval[:content], eval: p_eval[:eval], card_id: params[:id], user_id: current_user.id)
+      redirect_to "/cards/#{params[:id]}"
+    end 
 end
