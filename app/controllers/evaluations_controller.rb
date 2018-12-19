e557d7f10a9fc8e1ae5e232c7a1bb6bb37b93c83
Olivier <orohellec44@gmail.com>
2018-12-19 02:40:16 +0100

    a user can't comment twice the same activity

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,22 +1,23 @@
 class EvaluationsController < ApplicationController
-  
-    def index 
-      @evaluations = Evaluation.all 
-    end 
+
+    def index
+      @evaluations = Evaluation.all
+    end
 
     def show
       @evaluation = Evaluation.find(params[:id])
     end
 
-    def new 
-      @evaluation = Evaluation.new 
-    end 
+    def new
+      @evaluation = Evaluation.new
+    end
 
     def create
       p_eval = params[:evaluation]
       # search in the database Evaluations if the user has already done a comment on this card
-      (Evaluation.where(user_id: current_user.id, card_id: params[:id]).empty?)
-      @evaluation = Evaluation.create(comment: p_eval[:content], eval: p_eval[:eval], card_id: params[:id], user_id: current_user.id)
-      redirect_to "/cards/#{params[:id]}"
-    end 
+      if (Evaluation.where(user_id: current_user.id, card_id: params[:id]).empty?)
+        @evaluation = Evaluation.create(comment: p_eval[:content], eval: p_eval[:eval], card_id: params[:id], user_id: current_user.id)
+        redirect_to "/cards/#{params[:id]}"
+      end
+    end
 end
